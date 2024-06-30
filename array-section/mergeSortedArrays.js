b349d5bef1ea3254ebebc21578c1c1b39993c3cb
Megha Banerjee <60307904+MEGHA2507@users.noreply.github.com>
2024-06-30 08:46:54 +0000

    optimized code for mergeSortedArrays

--- array-section/mergeSortedArrays.js
+++ array-section/mergeSortedArrays.js
@@ -4,19 +4,19 @@ alert('mergeSortedArrays');
 
 
 // brute force solution
-function mergeSortedArrays(arr1, arr2){
-  if(arr1.length === 0){
+function mergeSortedArrays(arr1, arr2) {
+  if (arr1.length === 0) {
     return arr2;
   }
-  if(arr2.length === 0){
+  if (arr2.length === 0) {
     return arr1;
   }
 
   let mergedArr = arr1.concat(arr2);
 
-  for(let i=0; i<mergedArr.length; i++){
-    for(let j=i+1; j<mergedArr.length; j++){
-      if(mergedArr[i] > mergedArr[j]){
+  for (let i = 0; i < mergedArr.length; i++) {
+    for (let j = i + 1; j < mergedArr.length; j++) {
+      if (mergedArr[i] > mergedArr[j]) {
         let temp = mergedArr[i];
         mergedArr[i] = mergedArr[j];
         mergedArr[j] = temp;
@@ -27,6 +27,40 @@ function mergeSortedArrays(arr1, arr2){
 }
 
 // solutions with better time complexity
+function mergeSortedArrays2(arr1, arr2) {
+  const mergedArr = [];
+  let array1Item = arr1[0];
+  let array2Item = arr2[0];
+  let i = 1;
+  let j = 1;
+
+  if (arr1.length === 0) {
+    return arr2;
+  }
+
+  if (arr2.length === 0) {
+    return arr1;
+  }
+
+  while (array1Item || array2Item) {
+
+    if (!array2Item || array1Item < array2Item) {
+      mergedArr.push(array1Item);
+      array1Item = arr1[i];
+      i++;
+    } else {
+      mergedArr.push(array2Item);
+      array2Item = arr2[j];
+      j++;
+    }
 
 
-console.log( mergeSortedArrays([0,3,4,31], [4,6,30]));
+  }
+
+  return mergedArr;
+
+}
+
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+console.log(mergeSortedArrays2([0, 3, 4, 31], [4, 6, 30]));
