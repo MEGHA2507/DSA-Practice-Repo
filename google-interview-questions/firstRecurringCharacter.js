bac4702504e84a104f5bff41380f1e0955526768
Megha Banerjee <megha.banerjee07@gmail.com>
2025-01-26 02:05:00 +0530

    first recurring charcater -google questions

---
+++ google-interview-questions/firstRecurringCharacter.js
@@ -0,0 +1,30 @@
+// Google Questions
+
+// we have to find and return the first recurring character of the array.
+// Given an array = [2,5,1,2,3,5,1,2,4];
+// It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4];
+// It should return 1
+
+// Given an array = [2,3,4,5];
+// It should return undefined
+
+function firstRecurrCharacter(array) {
+  let mapObj = {};
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (mapObj[array[i]]) {
+      return array[i];
+    } else {
+      mapObj[array[i]] = count + 1;
+    }
+  }
+
+  return undefined;
+}
+
+console.log(firstRecurrCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurrCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurrCharacter([2, 3, 4, 5]));
